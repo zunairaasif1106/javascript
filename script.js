33a8ab000fbfbb87a0769487fa7a1e515796244e
zunaira asif <zunairaasif1106@gmail.com>
2025-10-31 19:08:27 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+alert("Welcome to my website! Have a nice day!");
+alert("Error! Please enter a valid password.");
+alert("Welcome to JS Land <br> Happy Coding!");
+alert("Welcome to JS Land...");
+alert("Happy Coding! <br> Prevent this page from creating additional dialogs.");
+console.log("Hello... I can run JS through my web browser's console.");
