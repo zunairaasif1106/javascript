bb4d5dbac003914f9e2cf77fe7c1cc8ffd44b4a4
zunairaasif1106 <zunairaasif1106@gmail.com>
2025-12-26 19:00:38 +0500

    assignmet 21

--- app.js
+++ app.js
@@ -1,61 +1,66 @@
-//chapter
-//question 1 
-let num = +(prompt("enter a positive number"));
-document.write("number" + num + "<br>")
-document.write("round off value of the number" + Math.round(num) + "<br>");
-document.write("Floor value: " + Math.floor(num) + "<br>");
-document.write("Ceil value: " + Math.ceil(num));
-
-//question 2
-let no = +(prompt("Enter a negative number:"));
-document.write("Number: " + no + "<br>");
-document.write("Round off value: " + Math.round(no) + "<br>");
-document.write("Floor value: " + Math.floor(no) + "<br>");
-document.write("Ceil value: " + Math.ceil(no));
-
-//question 3
- let absnum = +prompt("Enter a number:");
- document.write("Number: " + absnum + "<br>");
- document.write("Absolute value: " + Math.abs(absnum));
-
-//question 4
-let dice = Math.floor(Math.random() * 6) + 1;
-document.write("Dice Value: " + dice);
-
-//question 5
-let random = Math.floor(Math.random() * 100) + 1;
-document.write("Random Number: " + random);
-
-//chapter
-//question 1
+//chapter 35-38
+//q 1
 let Date = new Date();
 document.write(currentDate);
 
-//question 2
-let month = new Date();
-let newMonth = now.getDay();
-document.write(newMonth);
+//q 2
+let fname = propmt("enter your first name");
+let lname = prompt("enter your last name");
+let fullname = fname + " " + lname;
+alert ("welcome" + " " + fullname);
 
-//question 3
-let today = new Date();
-let day = day.getDay();
-alert("Today is: " + day);
+//q3
+function add() {
+let num1 = +prompt("Enter first number:");
+let num2 = +prompt("Enter second number:");
+return num1+num2
+}
+let a = add();
+alert (a);
 
-//question 4
-let day = new Date();
-let date = today.getDate();
-if (date < 16) {
-    document.write("First fifteen days of the month");
- } else {
-    document.write("Last days of the month");
-};
+//q4
+function calc(num1, num2, opr) {
+  if (opr === "+") {
+    return num1 + num2;
+  } else if (opr === "-") {
+    return num1 - num2;
+  } else if (opr === "*") {
+    return num1 * num2;
+  } else {
+    return "Incorrect operator!";
+  }
+}
+let fnum = +prompt("Enter first number:");
+let snum = +prompt("Enter second number:");
+let operator = prompt("Enter operator (+, -, *):");
 
-//question 5
-let now = new Date();
-let hour = now.getHours();
-    if (hour < 12) {
-        alert("It's AM");
-    } else {
-        alert("It's PM");
-    };
+alert(calc(fnum, snum, operator)
+);
 
+//q5
+function square (num){
+    return num * num;
+}
+let number = +prompt ("enter a number for square")
+alert (number + square(number));
+
+//q6
+function factorial(n) {
+  let result = 1;             
+  for (let i = 1; i <= n; i++) {  
+    result = result * i;      
+  }
+  return result;              
+}
+
+let num = +prompt("Enter a number:");
+alert( factorial(number));
+
+//q7
+function area(width, height) {
+  return width * height;
+}
+let area1 = area(12, 9); 
+alert(area1);
+
+//q8
