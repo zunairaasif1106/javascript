5e006597aa24bdce7b745585f28bf67703c53c9a
zunairaasif1106 <zunairaasif1106@gmail.com>
2026-01-02 15:36:24 +0500

    assignment 21

--- app.js
+++ app.js
@@ -1,66 +1,76 @@
-//chapter 35-38
-//q 1
-let Date = new Date();
-document.write(currentDate);
+//chapter 38-42
+//functions-switch-while.. do-while
 
-//q 2
-let fname = propmt("enter your first name");
-let lname = prompt("enter your last name");
-let fullname = fname + " " + lname;
-alert ("welcome" + " " + fullname);
-
-//q3
-function add() {
-let num1 = +prompt("Enter first number:");
-let num2 = +prompt("Enter second number:");
-return num1+num2
+//task 1
+function power(a, b) {
+    return(a, b);
 }
-let a = add();
-alert (a);
+console.log(power(4,4 ));
 
-//q4
-function calc(num1, num2, opr) {
-  if (opr === "+") {
-    return num1 + num2;
-  } else if (opr === "-") {
-    return num1 - num2;
-  } else if (opr === "*") {
-    return num1 * num2;
-  } else {
-    return "Incorrect operator!";
-  }
+//task 2
+function leapYear(year) {
+    if(year % 4 === 0) {
+        return year + " is a leap year";
+    } else {
+        return year + " is not a leap year";
+    }
 }
-let fnum = +prompt("Enter first number:");
-let snum = +prompt("Enter second number:");
-let operator = prompt("Enter operator (+, -, *):");
+console.log(leapYear(2020));
 
-alert(calc(fnum, snum, operator)
-);
 
-//q5
-function square (num){
-    return num * num;
+//task 3
+function area(a, b, c) {
+    let area = Math.sqrt(s * (s - a) * (s - b) * (s - c));
+    let s = (a + b + c) / 2;
+    return area;
 }
-let number = +prompt ("enter a number for square")
-alert (number + square(number));
+console.log(area(13, 24, 5));
 
-//q6
-function factorial(n) {
-  let result = 1;             
-  for (let i = 1; i <= n; i++) {  
-    result = result * i;      
-  }
-  return result;              
+//task 4
+function main (mark1, mark2, mark3){
+let average = average (mark1, mark2, mark3);
+let percentage = percentage (mark1, mark2, mark3);
+return  average +  percentage ;
+}
+function average (mark1, mark2, mark3){
+return (mark1 + mark2 + mark3) / 3;
+}
+function percentage (mark1, mark2, mark3){
+let total= 300;
+let obtained = mark1 + mark2 + mark3;
+return (obtained / total) * 100;
 }
 
-let num = +prompt("Enter a number:");
-alert( factorial(number));
+console.log(main(85, 90, 95));
 
-//q7
-function area(width, height) {
-  return width * height;
+//task 5
+function index(a, b) {
+    for (let i = 0; i < a.length; i++) {
+        if (a[i] === b) {
+            return i;
+        }
+    }
+    return -1;
 }
-let area1 = area(12, 9); 
-alert(area1);
+console.log(index("welcome", "e"));
 
-//q8
+//chapter 43-48
+//events
+//task 1
+function showAlert() {
+    alert("thanks for click");
+}
+
+//task 2
+// Display some Mobile images in browser. On click on an
+// image Show the message in alert to user.
+function image() {
+    alert("thanks for purchase a phone from us");
+}
+
+//task 3
+function deleteRow(r) {}
+
+//task 4
+//index.html
+
